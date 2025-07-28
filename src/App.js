8e500853363cb4d596c1eb0186ebefb49f1f2d05
scplasenciac <sebastianplasencia313@gmail.com>
2025-07-28 18:00:46 -0500

    Fix React Router warnings and update test to pass successfully

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true
+      }}
+    >
       <div className="App">
         <Layout>
           <Routes>
